df5bc5ca543c3971f71d483728007e060f31d003
libing <liwubing@huizhaofang.com>
2021-05-30 21:45:54 +0800

    暂时提交，后面补充作业内容

---
+++ demo04/src/test/java/com/task/demo04/Demo04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.demo04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
